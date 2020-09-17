ca8238568adf24280117a6530fd09b1d01fc6827
Jared <jaredarodriguez@gmail.com>
2020-09-17 09:24:23 -0400

    unsplash data fetched

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+// Unsplash API
+const count = 10; 
+const apiKey = '9t9bCWY_CyxVohP7lGl8Oq8vc-rlbtDW1wRjTHCYfzY';
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API 
+async function getPhotos() {
+    try {
+        const response = await (fetch(apiURL));
+        const data = await response.json(); 
+        console.log(data);
+    } catch (error) {
+        // Catch error
+    }
+}
+
+// On Load
+getPhotos();
