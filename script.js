0eff65dad750ea22dffe5f6eccc5e64f1e6e2ad1
Jared <jaredarodriguez@gmail.com>
2020-09-18 09:18:32 -0400

    unsplash images rendering

--- script.js
+++ script.js
@@ -8,12 +8,30 @@ const count = 10;
 const apiKey = '9t9bCWY_CyxVohP7lGl8Oq8vc-rlbtDW1wRjTHCYfzY';
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Create Elements For Links & Photos, Add, to DOm 
+function displayPhotos() {
+    photosArray.forEach((photo) => {
+        //  Create <a> to link to Unsplash 
+        const item = document.createElement('a'); 
+        item.setAttribute('href', photo.links.html); 
+        item.setAttribute('target', '_blank'); 
+        //  Create <img> for photo
+        const img = document.createElement('img'); 
+        img.setAttribute('src', photo.urls.regular); 
+        img.setAttribute('alt', photo.alt_description); 
+        img.setAttribute('title', photo.alt_description); 
+        //  Put <img> inside <a>, then put both inside imageContainer element 
+        item.appendChild(img); 
+        imageContainer.appendChild(item); 
+    }); 
+}
+
 // Get photos from Unsplash API 
 async function getPhotos() {
     try {
         const response = await (fetch(apiURL));
         photosArray = await response.json(); 
-        console.log(photosArray);
+        displayPhotos();
     } catch (error) {
         // Catch error
     }
