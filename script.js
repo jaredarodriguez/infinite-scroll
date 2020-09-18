7c0535bab8de9b6cd593e362111c0776169c2caf
Jared <jaredarodriguez@gmail.com>
2020-09-18 09:26:14 -0400

    set attribute helper function

--- script.js
+++ script.js
@@ -8,18 +8,30 @@ const count = 10;
 const apiKey = '9t9bCWY_CyxVohP7lGl8Oq8vc-rlbtDW1wRjTHCYfzY';
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+//  Helper function to Set Attributes on DOM Elements 
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]); 
+    }
+}
+
 // Create Elements For Links & Photos, Add, to DOm 
 function displayPhotos() {
     photosArray.forEach((photo) => {
         //  Create <a> to link to Unsplash 
         const item = document.createElement('a'); 
-        item.setAttribute('href', photo.links.html); 
-        item.setAttribute('target', '_blank'); 
+        setAttributes(item, {
+            href: photo.links.html, 
+            target: '_blank', 
+
+        })
         //  Create <img> for photo
         const img = document.createElement('img'); 
-        img.setAttribute('src', photo.urls.regular); 
-        img.setAttribute('alt', photo.alt_description); 
-        img.setAttribute('title', photo.alt_description); 
+        setAttributes(img, {
+            src: photo.urls.regular, 
+            alt: photo.alt_description, 
+            title: photo.alt_description
+        })
         //  Put <img> inside <a>, then put both inside imageContainer element 
         item.appendChild(img); 
         imageContainer.appendChild(item); 
